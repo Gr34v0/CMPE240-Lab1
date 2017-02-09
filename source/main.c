880bff094fa54cda856e9ff5d042f2125e7a34ec
Christian Greaves <gr34v0@gmail.com>
2017-02-08 20:21:24 -0500

    first time doing anything

--- source/main.c
+++ source/main.c
@@ -11,23 +11,34 @@
 // to satisfy timing constraints given in lab. 
 // ....beware compiler optimization....
 void delay(uint32_t count)
-{
-	
+{   
+    uint32_t x = 0;
+	while (x < count) //loop x times
+             // 1200MHz. 4 instructions/cycle. loop 40 cycles
+    {
+        x = x + 1;
+    } 
 }
 
 int main()
 {
-	// Select output mode and which pin to drive
+	// Select output mode and which pin to Drive
+    GPIO[GPFSEL1] |= (001 << 21);
         
     while (1)
     {       
         //toggle clear register for the chosen pin
-        
+        // GPIO17 bits 23-21  bit 001 = output
+        GPIO[GPCLR0] |= (1 << 17);
+
         //apply a delay
-        
+        delay(160);
+
         //toggle set register for the chosen pin
-        
+        GPIO[GPSET1] |= (0 << 17);
+
         //apply a delay
+        delay(160);
     }
     
     return 0;
